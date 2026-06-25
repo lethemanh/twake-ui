3f48a97006c0aa877a18d72fd932585b845f45e1
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-06-25 14:45:40 +0200

    fix: Missed some props on SVG Icons (Cube and Spinner)

--- packages/twake-icons/src/Icons/Cube.tsx
+++ packages/twake-icons/src/Icons/Cube.tsx
@@ -1,9 +1,9 @@
 // Automatically created, please run `scripts/generate-svgr.sh ` to regenerate;
 import * as React from "react";
 import type { SVGProps } from "react";
-function SvgCube(props: SVGProps<SVGSVGElement>) {
+function SvgCube({ iconRef, ...props }: SVGProps<SVGSVGElement> & { iconRef?: React.Ref<SVGSVGElement> }) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 16" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 16" {...props} ref={iconRef}>
       <path d="M14.4 4 7.2 0 0 4v8l7.2 4 7.2-4zM7.2 1.832l4.728 2.624L9.52 5.792a3.213 3.213 0 0 0-4.64 0L2.472 4.456zm-.8 11.896-4.8-2.664V5.808L4.104 7.2A3.193 3.193 0 0 0 6.4 11.096zM5.6 8c0-.88.72-1.6 1.6-1.6s1.6.72 1.6 1.6-.72 1.6-1.6 1.6S5.6 8.88 5.6 8M8 13.728v-2.624a3.19 3.19 0 0 0 2.4-3.096q.002-.422-.104-.808L12.8 5.808v5.256z" />
     </svg>
   );
